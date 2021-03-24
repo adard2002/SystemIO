9eb2c0ba3f396b1f51bc6d7ce40df6a2555dca45
unknown <adard2002@gmail.com>
2021-03-24 16:27:16 -0500

    added sln and console

---
+++ SysIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
